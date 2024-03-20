67352eed1e19f729edf3e8691b4c1f873fc2d9b7
marina-beppler <marina.beppler@ufpr.br>
2024-03-19 21:07:09 -0300

    Ajustes de estilo e implementação modal de senha

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import Login  from '../views/Login.vue';
 import Cadastro from '../views/Cadastro.vue';
+import forgotPassword from '../views/forgotPasswordModal.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -23,6 +24,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/cadastro',
     name: 'Cadastro',
     component: Cadastro
+  },
+  {
+    path: '/forgotPassword',
+    name: 'forgotPassword',
+    component: forgotPassword
   }
 ]
 

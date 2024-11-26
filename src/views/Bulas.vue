21c87c25a4bbd601320461c73723c786fc573887
Marina Beppler <marina.beppler@ufpr.br>
2024-11-26 14:39:23 -0300

    mudança api bulario

--- src/views/Bulas.vue
+++ src/views/Bulas.vue
@@ -1,151 +1,140 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import axios from 'axios';
-import { IonHeader, IonToolbar, IonButton, IonButtons, IonIcon, IonInput, IonItem, IonCard, IonPage, IonContent, toastController, IonLabel, IonList, IonNote } from '@ionic/vue';
-import { useRouter } from 'vue-router';
+import {
+  IonHeader, IonToolbar, IonButton, IonButtons, IonIcon, IonInput, IonItem,
+  IonCard, IonPage, IonContent, toastController, IonLabel, IonList, IonNote
+} from '@ionic/vue';
 import { searchOutline } from 'ionicons/icons';
 import NavigationMenu from '../views/components/NavigationMenu.vue';
-import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
-
-
-interface Medicamento {
-    idProduto: number;
-    nomeProduto: string;
-    idBulaPacienteProtegido: string;
-    idBulaProfissionalProtegido: string;
-}
 
 export default defineComponent({
-    name: "Bulas",
-    components: {
-        IonHeader,
-        IonToolbar,
-        IonButton,
-        IonIcon,
-        IonButtons,
-        IonInput,
-        IonItem,
-        IonCard,
-        IonPage,
-        IonContent,
-        NavigationMenu,
-        IonLabel,
-        IonList,
-        IonNote
-    },
-    setup() {
-        const medicamento = ref('');
-        const pagina = ref(1);
-        const searchOutlineIcon = searchOutline;
-        const searchResults = ref<Medicamento[]>([]);
+  name: "Bulas",
+  components: {
+    IonHeader, 
+    IonToolbar, 
+    IonButton, 
+    IonIcon, 
+    IonButtons, 
+    IonInput,
+    IonItem, 
+    IonCard, 
+    IonPage, 
+    IonContent, 
+    NavigationMenu, 
+    IonLabel, 
+    IonList, 
+    IonNote
+  },
+  setup() {
+    const medicamento = ref('');
+    const splSetId = ref('');
+    const searchResults = ref<any[]>([]);
+    const splData = ref<any | null>(null); 
+    const searchOutlineIcon = searchOutline;
 
-        const presentToast = async (message: string) => {
-            const toast = await toastController.create({
-                message: message,
-                duration: 1500
-            });
-            toast.present();
-        };
+    const presentToast = async (message: string) => {
+      const toast = await toastController.create({
+        message,
+        duration: 1500
+      });
+      toast.present();
+    };
 
-        const searchMedicamento = async () => {
-            if (!medicamento.value) {
-                presentToast('Por favor, digite o nome do medicamento.');
-                return;
-            }
-            try {
-                const response = await axios.get('https://bula.vercel.app/pesquisar', {
-                    params: {
-                        nome: medicamento.value,
-                        pagina: pagina.value
-                    }
-                });
-                searchResults.value = response.data.content.slice(0, 3); 
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                presentToast('Erro pesquisando medicamento!');
-            }
-        };
+    const searchMedicamento = async () => {
+      if (!medicamento.value) {
+        presentToast('Por favor, digite o nome do medicamento.');
+        return;
+      }
+      try {
+        const response = await axios.get(`https://api.fda.gov/drug/ndc.json?search=brand_name:${medicamento.value}&limit=3`);
+        if (response.data.results && response.data.results.length > 0) {
+          searchResults.value = response.data.results;
+          splSetId.value = response.data.spl_id;
+          presentToast('id ok, ${splSetId}');
+        } else {
+          searchResults.value = [];
+          presentToast('Nenhum medicamento encontrado.');
+        }
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+        presentToast('Erro ao buscar medicamento!');
+      }
+    };
 
-        const blobToBase64 = (blob: Blob): Promise<string> => {
-            return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    const base64String = (reader.result as string).split(',')[1];
-                    resolve(base64String);
-                };
-                reader.onerror = reject;
-                reader.readAsDataURL(blob);
-            });
-        };
+    const fetchSPLData = async (splSetId: string) => {
+      try {
+        const response = await axios.get(`https://api.fda.gov/drug/ndc.json?search=spl_id:${splSetId}`);
+        if (response.data.results && response.data.results.length > 0) {
+          splData.value = response.data.results[0];
+        } else {
+          splData.value = null;
+          presentToast('Dados da bula não encontrados.');
+        }
+      } catch (error) {
+        console.error('Erro ao obter a bula do medicamento:', error);
+        presentToast('Erro ao obter a bula!');
+      }
+    };
 
-        const fetchBulaPDF = async (id: string) => {
-            try {
-                presentToast('Baixando bula, aguarde...');
-
-                const response = await axios.get('https://bula.vercel.app/pdf', {
-                    params: { id: id },
-                    responseType: 'blob'
-                });
-
-                const base64String = await blobToBase64(response.data);
-
-                await Filesystem.writeFile({
-                    path: 'bula.pdf',
-                    data: base64String,
-                    directory: Directory.Documents
-                });
-
-                presentToast('Bula salva com sucesso!');
-            } catch (error) {
-                console.error('Error fetching PDF:', error);
-                presentToast('Erro ao obter a bula do medicamento!');
-            }
-        };
-
-
-        return {
-            medicamento,
-            pagina,
-            searchOutlineIcon,
-            searchMedicamento,
-            searchResults,
-            fetchBulaPDF
-        };
-    }
+    return {
+      medicamento,
+      searchOutlineIcon,
+      searchMedicamento,
+      searchResults,
+      splData,
+      fetchSPLData
+    };
+  }
 });
 </script>
 
 <template>
-    <ion-page>
-        <ion-header>
-            <NavigationMenu />
-        </ion-header>
-        <ion-content>
-            <ion-card id="background">
-                <ion-card class="card">
-                    <div id="pesquisar">
-                        <ion-item id="pesquisar" lines="none">
-                            <ion-button @click="searchMedicamento">
-                                <ion-icon :icon="searchOutlineIcon" slot="start" size="large"></ion-icon>
-                            </ion-button>
-                            <ion-input v-model="medicamento" placeholder="Digite o nome do medicamento" required></ion-input>
-                        </ion-item>
-                    </div>
-                    <p>Clique no campo acima para pesquisar digitar o nome do medicamento desejado. Para pesquisar, clique na lupa!</p>
-                    <div v-if="searchResults.length">
-                        <ion-list>
-                            <ion-item v-for="med in searchResults" :key="med.idProduto">
-                                <ion-label @click="fetchBulaPDF(med.idBulaPacienteProtegido)">
-                                    <h3>{{ med.nomeProduto }}</h3>
-                                    <ion-note>ID: {{ med.idProduto }}</ion-note>
-                                </ion-label>
-                            </ion-item>
-                        </ion-list>
-                    </div>
-                </ion-card>
+  <ion-page>
+    <ion-header>
+      <NavigationMenu />
+    </ion-header>
+    <ion-content>
+      <ion-card id="background">
+        <ion-card class="card">
+          <div id="pesquisar">
+            <ion-item id="pesquisar" lines="none">
+              <ion-button @click="searchMedicamento">
+                <ion-icon :icon="searchOutlineIcon" slot="start" size="large"></ion-icon>
+              </ion-button>
+              <ion-input v-model="medicamento" placeholder="Digite o nome do medicamento" required></ion-input>
+            </ion-item>
+          </div>
+          <p>Clique no campo acima para pesquisar o medicamento desejado. Para pesquisar, clique na lupa!</p>
+          
+          <div v-if="searchResults.length">
+            <ion-list>
+              <ion-item v-for="med in searchResults" :key="med.spl_id" @click="fetchSPLData(med.spl_id)">
+                <ion-label>
+                  <h3>{{ med.brand_name }}</h3>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </div>
+
+          <!-- Display SPL Data -->
+          <div v-if="splData">
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>{{ splData.brand_name }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <p><strong>Ingrediente ativo</strong> {{ splData.active_ingredient }}</p>
+                <p><strong>Descrição:</strong> {{ splData.product_label }}</p>
+                <p><strong>Indicação:</strong> {{ splData.indication }}</p>
+                <!-- Add more fields based on available SPL data -->
+              </ion-card-content>
             </ion-card>
-        </ion-content>
-    </ion-page>
+          </div>
+        </ion-card>
+      </ion-card>
+    </ion-content>
+  </ion-page>
 </template>
 
 <style scoped>
